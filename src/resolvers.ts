01db4fd68a270526be14f4bff401282db9e210a1
SonuKumar1021 <rsonu0195@gmail.com>
2025-06-06 12:07:15 +0530

    "created resolver function to get particular game, author and review"

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -3,7 +3,16 @@ import db from "./_db";
 export const resolvers = {
   Query: {
     games: () => db.games,
+    game: (_: any, args: {id: string}) => {
+      return db.games.find(game => game.id === args.id);
+    },
     authors: () => db.authors,
+    author: (_: any, args: {id: string}) => {
+      return db.authors.find(author => author.id === args.id);
+    },
     reviews: () => db.reviews,
+    review: (_: any, args: {id: string}) => {
+      return db.reviews.find(review => review.id === args.id);
+    },
   },
 };
