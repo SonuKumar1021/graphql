c90e115177eda1dbce7b24f5f1b159924059ebe2
SonuKumar1021 <rsonu0195@gmail.com>
2025-06-06 19:12:36 +0530

    Created schema and resolver functions for related data

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -15,4 +15,22 @@ export const resolvers = {
       return db.reviews.find(review => review.id === args.id);
     },
   },
+  Review: {
+    game: (parent: {game_id: string}) => {
+      return db.games.find(game => game.id === parent.game_id);
+    },
+    author: (parent: {author_id: string}) => {
+      return db.authors.find(author => author.id === parent.author_id);
+    },
+  },
+  Game: {
+    gameReviews: (parent: {id: string}) => {
+      return db.reviews.filter(review => review.game_id === parent.id);
+    },
+  },
+  Author: {
+    authorReviews: (parent: {id: string}) => {
+      return db.reviews.filter(review => review.author_id === parent.id);
+    },
+  },
 };
--- src/schema.ts
+++ src/schema.ts
@@ -3,26 +3,30 @@ type Game {
     id:ID!
     title:String!
     platform: [String!]!
+    gameReviews: [Review!]
 }
 
 type Author {
     id: ID!
     name: String!
     verified: Boolean!
+    authorReviews: [Review!]
 }
 
 type Review {
     id: ID!
     rating: Int!
     content: String!
+    game: Game!
+    author: Author!
 }
 
 type Query {
     games: [Game]
-    game(id:ID!): Game
+    game(id: ID!): Game
     authors: [Author]
-    author(id:ID!): Author
+    author(id: ID!): Author
     reviews: [Review]
-    review(id:ID!): Review
+    review(id: ID!): Review
 }
 `;
